5da375283f31d26fd6affd357fffd61844b7c1fe
Seth King <king9778@vandals.uidaho.edu>
2021-04-08 10:23:43 -0700

    Adjusted server script to run continuously and accept custom reply input

--- scripts/server.sh
+++ scripts/server.sh
@@ -6,6 +6,9 @@
 
 # This script is intended to be run on the server
 
+while [ 1 -eq 1 ]
+do
+
 echo Listening for message...
 python3 receivemessage.py 192.168.1.104 80
 mv temp.bin message.txt
@@ -13,7 +16,10 @@ mv temp.bin message.txt
 echo Message received! Message contents:
 cat message.txt
 
-echo "Message received: " > reply.txt
-cat message.txt >> reply.txt
+echo Enter a reply to Bob:
+read message
+echo 192.168.1.103 > reply.txt
+echo $message >> reply.txt
 sendmessage.py reply.txt 192.168.1.103 80
 
+done
